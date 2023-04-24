1bb0e46da283ab778b16062248972d8d510b9c24
Abdelrahman Mostafa <abdelrahmanmostafa785@gmail.com>
2023-04-24 02:06:57 +0200

    first commit

---
+++ views/student_profile.view.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="ar">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>الملف الشخصي</title>
+
+    <!--CDN Links-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <!--Stylesheet Files-->
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/student_profile.css">
+</head>
+<body>
+    <div class="main" style="margin-top: 100px">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-5 col-sm-12">
+                        <div class="profile-card">
+                            <div class="profileImg">
+                                <img src="https://elebdaa-academy.com/imgs/user.webp" alt="">
+                            </div>
+                            <div class="card-details">
+                                <div class="personalInformation">
+                                    <h3 class="profileName">م.عبدالرحمن مصطفى</h3>
+                                    <span class="profileType">سوبر ادمن</span>
+                                </div>
+                                <div class="someDetails">
+                                    <div class="container">
+                                        <div class="row">
+                                            <div class="col-12 someDetailsHeader">
+                                                معلومات الطالب
+                                            </div>
+                                            <div class="col-12">
+                                                <div class="item">
+                                                    <span class="type">الاسم:</span>
+                                                    <span class="info">م.عبدالرحمن مصطفى</span>
+                                                </div>
+                                            </div>
+                                            <div class="col-12">
+                                                <div class="item">
+                                                    <span class="type">الاسم:</span>
+                                                    <span class="info">م.عبدالرحمن مصطفى</span>
+                                                </div>
+                                            </div>
+                                            <div class="col-12">
+                                                <div class="item">
+                                                    <span class="type">الاسم:</span>
+                                                    <span class="info">م.عبدالرحمن مصطفى</span>
+                                                </div>
+                                            </div>
+                                            <div class="col-12">
+                                                <div class="item">
+                                                    <span class="type">الاسم:</span>
+                                                    <span class="info">م.عبدالرحمن مصطفى</span>
+                                                </div>
+                                            </div>
+                                            <div class="col-12">
+                                                <div class="item">
+                                                    <span class="type">الاسم:</span>
+                                                    <span class="info">م.عبدالرحمن مصطفى</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-7 col-sm-12">
+
+                    </div>
+                </div>
+            </div>
+    </div>
+<!--CDN Links-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
