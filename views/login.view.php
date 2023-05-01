03884fc1c233693821aecdcc5e6a753a70243b1e
Abdelrahman Mostafa <abdelrahmanmostafa785@gmail.com>
2023-05-01 17:03:25 +0300

    finish login page

--- views/login.view.php
+++ views/login.view.php
@@ -11,6 +11,8 @@
     <link rel="stylesheet" href="assets/css/login.css">
     <!-- Google Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <script src="https://kit.fontawesome.com/f2178052c7.js" crossorigin="anonymous"></script>
+
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@500;600&display=swap" rel="stylesheet">
 </head>
@@ -32,18 +34,25 @@
             <p>تسجيل الدخول</p>
             <span></span>
             <!-- Start Form -->
-            <form class="g-3 needs-validation" novalidate>
+            <form class="g-3 needs-validation" novalidate action="/login" method="POST">
                 <div class="input-group has-validation">
                     <i class="fa-solid fa-envelope "></i>
-                    <input class="form-control " id="validationCustomUsername" type="email" placeholder="ادخل بريدك الالكتروني" aria-describedby="inputGroupPrepend" required>
+                    <input class="form-control " id="validationCustomUsername" type="email" placeholder="ادخل بريدك الالكتروني" aria-describedby="inputGroupPrepend" name="email" required>
                 </div>
                 <div class="input-group has-validation">
                     <i class="fa-solid fa-lock"></i>
-                    <input class="form-control"  type="password" placeholder="ادخل كلمه المرور" aria-label="default input example" required>
+                    <input class="form-control"  type="password" placeholder="ادخل كلمه المرور" aria-label="default input example" name="password" required>
                 </div>
-                <div class="alert alert-danger" role="alert">
-                    A simple danger alert—check it out!
-                </div>
+                <?php if(isset($errors['email'])): ?>
+                    <div class="alert alert-danger" role="alert">
+                        <?php echo $errors['email']; ?>
+                    </div>
+                <?php endif;?>
+                <?php if(isset($errors['password'])): ?>
+                    <div class="alert alert-danger" role="alert">
+                        <?php echo $errors['password']; ?>
+                    </div>
+                <?php endif;?>
                 <div>
                     <button class="btn btn-primary" type="submit">تسجيل</button>
                 </div>
@@ -59,6 +68,6 @@
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 <!-- JS Min -->
-<script src="js/login.js"></script>
+<script src="assets/js/login.js"></script>
 </body>
 </html>
