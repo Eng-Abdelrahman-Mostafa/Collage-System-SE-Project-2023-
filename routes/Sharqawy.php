23f3d7146e4a39b82c53df7a152f373904649979
Abdelrahman Mostafa <abdelrahmanmostafa785@gmail.com>
2023-05-10 03:57:02 +0300

    add routes

--- routes/Sharqawy.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-
-
-$router->get('/add_student',  'app\http\Controllers\Controller_add_student.php', 'index');
-$router->post('/addStudent', 'app\http\Controllers\Controller_add_student.php', 'submit');
--- routes/routes_provider.php
+++ routes/routes_provider.php
@@ -1,9 +1,4 @@
 <?php
 require 'web.php';
 require 'api.php';
-require 'aya.php';
-require 'AbdoFahd.php';
-require  'Sharqawy.php';
-
-require 'abdalla_elshamy.php';
 
